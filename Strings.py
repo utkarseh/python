4f295cb789035b951a39c3e8fdf76939762cae4a
utkarseh <utkarseh@gmail.com>
2026-08-18 17:57:48 +0530

    STRINGS

---
+++ Strings.py
@@ -0,0 +1,10 @@
+firstName = "UTKARSH"
+lastName = "MISHRA"
+fullName = firstName + " " + lastName
+print(fullName)
+random = "Utkarsh jewkgfffeiuwfgkuwueif"
+
+print(random[6])
+#starts from 0 so it is giving 11th posn alphabet on writing 10th so use n-1
+print(random[0:7])
+print(random[3:]) #it will print 0 to 7 but with a step of 2
