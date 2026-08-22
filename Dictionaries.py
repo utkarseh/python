31a95190f5cc8a12ea6ad77debae3c5db702f24a
utkarseh <utkarseh@gmail.com>
2026-08-22 19:56:47 +0530

    DICTIONARIES

---
+++ Dictionaries.py
@@ -0,0 +1,6 @@
+students = {"Alice": 85, "Bob": 92, "Charlie": 78, "David": 90}
+#print(students["Charlie"])
+students["Charlie"] = 999
+print(students["Charlie"])
+del students["Charlie"]
+print(students)
