b927e8c0c273f483a43b40b445f381df5b739c10
utkarseh <utkarseh@gmail.com>
2026-08-23 13:47:05 +0530

    TUPPLES

---
+++ Tuples.py
@@ -0,0 +1,11 @@
+tup1 = ("Maths",29,"apple")
+#it doesnt support append and del fxns
+print(tup1)
+tup1 = (1 ,2,3)
+print(tup1)
+cs = tup1[0]
+print(cs)
+cse = tup1[1:3]
+print(cse)
+del tup1
+print(tup1)
