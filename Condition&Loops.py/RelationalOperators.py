199709e2edc6c225fb1a5f8feb9d929f73c8b83f
utkarseh <utkarseh@gmail.com>
2026-08-23 18:15:10 +0530

    RELATIONAL OPERATORS

---
+++ Condition&Loops.py/RelationalOperators.py
@@ -0,0 +1,14 @@
+if 5>2:
+    print("Halllooo")
+num = 67
+print(num>50)
+print(num<50)
+print(num>=67)
+print(num<=67)
+
+if 5 == 69:
+    print("HAHAHAHAHA")
+else:
+    print("BHAGOOO")
+# = is used for assignment, while == is used for comparison.
+    
