6dae32be045133fae7e6ecc9e658a706a24209d3
utkarseh <utkarseh@gmail.com>
2026-08-23 15:31:13 +0530

    updated project

---
+++ Section2exercises.py
@@ -0,0 +1,19 @@
+#QUESTION 1
+Songs = ["REFLECTIONS","EARRINGS","LOVE STORY","MOCKING BIRD"]
+Movies = ["TITANIC","AVENGERS","SPIDERMAN","BATMAN"]
+LIST3 = Songs + Movies
+print(LIST3)
+LIST3.append("HARRY POTTER")
+print(LIST3)
+
+#QUESTION 2
+x = 20-10+3+15-5
+y = 15*3
+c = x+y
+print(c)
+
+#QUESTION 3
+varFirstname = input("Enter your first name: ")
+varLastname = input("Enter your last name: ")
+greetings = ("Hello there %s %s" % (varFirstname, varLastname))
+print(greetings)
