5c79771b077d8f0036d134bdc7ed9ca5c7d5c0f4
utkarseh <utkarseh@gmail.com>
2026-09-03 22:56:31 +0530

    ELIFstatements-

---
+++ ConditionNLoops.py/Elifstatements.py
@@ -0,0 +1,12 @@
+day = "monday"
+if day == "sunday":
+    print("sunny")
+else:
+    print("not sunny")
+#to add more conditions we can use elif statements
+if day == "sunday":
+    print("sunny")
+elif day == "monday":
+    print("what a boring day")
+else:
+    print("not sunny")        
