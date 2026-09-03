f19f270b01d14dd2eb00c778ecac96b6400a56d3
utkarseh <utkarseh@gmail.com>
2026-09-03 22:48:01 +0530

    NestediFElse

---
+++ ConditionNLoops.py/NestedIfElse.py
@@ -0,0 +1,9 @@
+age = 18
+if age > 14: 
+     print("You are eligible for instagram")
+name = "Utkarsh"
+if age >14:
+     if name == "Utkarsh":
+                 print("You got the bluetick for free just becoz of your name now thank your parents")
+else:
+               print("You are not eligible for instagram")
