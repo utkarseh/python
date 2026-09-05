598f4106142c88b951ba8edf47a45286e37e00e8
utkarseh <utkarseh@gmail.com>
2026-09-05 23:58:12 +0530

    for loops

---
+++ ConditionNLoops.py/ForLoops.py
@@ -0,0 +1,10 @@
+for i in range(0,5):
+    print(i)
+shoppingList=["milk","eggs","bread"]
+for i in shoppingList:
+    print(i)
+tup=(2,3,6,7)
+for i in tup:
+    print(i)
+    for i in range(0,100000000000000000000000000000009,2):
+        print(i)
