eebb8aca45ccb85aafcbc12fcfd4779a4c8cad73
utkarseh <utkarseh@gmail.com>
2026-09-05 23:40:05 +0530

    logical operators

---
+++ ConditionNLoops.py/LogicalOperators.py
@@ -0,0 +1,11 @@
+
+age = 14 
+name = "babyboy"
+
+if age==14 and name=="babyboy":
+       print("ayayyyyayyayayyyayyayy")
+
+if age ==14 or name=="ladyboy":
+       print("Fuckyouuu")
+else:
+       print("hewll nahhhh mannn") 
