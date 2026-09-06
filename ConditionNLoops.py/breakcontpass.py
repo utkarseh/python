92744b7bdac2be69f86691bedd0ecdb9f7e1b581
utkarseh <utkarseh@gmail.com>
2026-09-06 13:07:42 +0530

    break pass and continue

---
+++ ConditionNLoops.py/breakcontpass.py
@@ -0,0 +1,16 @@
+counter=0
+while counter<100:
+    if counter==4:
+        break
+    else:
+        pass
+    print(counter)
+    counter+=1
+for i in "Python":
+    if i=="h":
+        continue
+    print(i)
+for i in range(0,10):
+    if i<5:
+        continue
+    print(i)
