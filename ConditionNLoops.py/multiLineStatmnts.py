e102e04b39286bc7e90fcc10827b38af1ca86930
utkarseh <utkarseh@gmail.com>
2026-09-06 14:00:57 +0530

    multiline statements

---
+++ ConditionNLoops.py/multiLineStatmnts.py
@@ -0,0 +1,11 @@
+print("it's a nice day")
+print('it\'s a nice day')
+#\ in front of a character is used to escape it and make it a normal character instead of a special character
+blueorangeswithcheese="a"
+cheesefrieddumplingswithsauce="b"
+if blueorangeswithcheese\
+    ==\
+        cheesefrieddumplingswithsauce:
+    print("Hello")
+else:
+    print("Heyyyaaa")
