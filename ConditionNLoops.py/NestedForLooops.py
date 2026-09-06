748efd8e43e788d516042aed09102752df07c152
utkarseh <utkarseh@gmail.com>
2026-09-06 12:35:27 +0530

    nestedforloops

---
+++ ConditionNLoops.py/NestedForLooops.py
@@ -0,0 +1,13 @@
+for i in range(0,5):
+    for a in range(0,5):
+            print(a)
+#prime number generator
+for num in range (2,101):
+      prime = True
+        for i in range(2):
+           if num%i==0:
+                prime=False
+                if prime:
+                     print(True)
+    
+                        
