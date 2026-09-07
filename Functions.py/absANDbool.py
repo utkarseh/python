d2b631204413edc454c256bd64e58341bfe10a3d
utkarseh <utkarseh@gmail.com>
2026-09-07 23:12:11 +0530

    boolean and absolute fxn

---
+++ Functions.py/absANDbool.py
@@ -0,0 +1,6 @@
+#AbsoluteFunction
+print(abs(-69))
+print(abs(67))
+#Boolean Function
+print(bool(0))
+print(bool(67))
--- Functions.py/globalNDlocalVars.py
+++ Functions.py/globalNDlocalVars.py
@@ -4,3 +4,5 @@ def multiply(num1,num2):
     return total
 print(multiply(5,6))
 total
+#local variable only exists inside the function and cannot be accessed outside the function.
+#global variable can be accessed anywhere in the program.
