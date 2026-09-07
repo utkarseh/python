c9fa01ae98b10fa5c45843ae8721dc95132caad5
utkarseh <utkarseh@gmail.com>
2026-09-07 22:52:00 +0530

    Making a Function

---
+++ ConditionNLoops.py/commenting.py
@@ -0,0 +1,7 @@
+#This is a comment
+#this can be used to organige your code and make it more readable and also to remove some code without actually removing it
+'''HEYY BROOO 
+JHSJHDU
+THOE
+FJIDOIJ'''
+#with the use of ''' you can write multi line comments
---
+++ Functions.py/ownFunctions.py
@@ -0,0 +1,10 @@
+for i in range(0,5):
+    print("Hi")
+
+def functionName():
+    for i in range(0,5):
+            print("Hi")
+functionName()
+def addNum(num1,num2):
+         return(num1+num2)
+print(addNum(69,67))
