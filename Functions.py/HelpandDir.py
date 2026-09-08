d46a27bebc8dad1bae9d694305c989eff355b791
utkarseh <utkarseh@gmail.com>
2026-09-08 23:46:30 +0530

    dir and help

---
+++ Functions.py/HelpandDir.py
@@ -0,0 +1,5 @@
+print(dir(["jhi"]))
+tiger = "i am a tiger"
+print(dir(tiger))
+print(tiger.upper)
+print(tiger.title)
--- Functions.py/absANDbool.py
+++ Functions.py/absANDbool.py
@@ -4,3 +4,4 @@ print(abs(67))
 #Boolean Function
 print(bool(0))
 print(bool(67))
+#Boolean function returns false only for 0, empty string, empty list, empty tuple, empty dictionary, and None. All other values return True.
