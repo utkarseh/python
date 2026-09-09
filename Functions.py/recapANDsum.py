13aa4fd9fc73a9a162a3733209ff4303322f5214
utkarseh <utkarseh@gmail.com>
2026-09-09 22:37:49 +0530

    sum func

---
+++ Functions.py/recapANDsum.py
@@ -0,0 +1,7 @@
+print(len("utkarsh"))
+array=("a", "b", "c", "d")
+print(max(array))
+array=("a", "b", "c", "d","A","B")
+print(min(array))
+list1 =[22,33,67,69]
+print(sum(list1))
---
+++ Functions.py/strIntFloat.py
@@ -0,0 +1,2 @@
+# i know all this 
+# it can convert fron one data type to another data type
