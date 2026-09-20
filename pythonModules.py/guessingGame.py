8ee634e415eefd40622a5d223570f12199417850
utkarseh <utkarseh@gmail.com>
2026-09-20 19:48:15 +0530

    guessing game

---
+++ pythonModules.py/guessingGame.py
@@ -0,0 +1,11 @@
+import random
+a=random.randint(0,100)
+while True:
+    b=int(input("guess a number between 1 and 100: "))
+    if b>a:
+        print("try lower number")
+    elif b<a:
+        print("try higher number")
+    else :
+        print("YOU WONN BITCHH")
+        break
