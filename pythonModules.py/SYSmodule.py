8b355ec049351f99ee6bdfdbced3e5369242ffce
utkarseh <utkarseh@gmail.com>
2026-09-21 11:44:33 +0530

    sys module read write version

--- pythonModules.py/SYSmodule.py
+++ pythonModules.py/SYSmodule.py
@@ -1,5 +1,7 @@
 import sys
-#inputStatement=sys.stdin.readline(10)
-#print(inputStatement)
+inputStatement=sys.stdin.readline(10)
+print(inputStatement)
 sys.stdout.write("this is a statement")
 print(sys.version)
+#headache
+print("headache")
