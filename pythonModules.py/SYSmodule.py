664b56500a2e83b9d2cc8cbf1e0046ca0800bbc1
utkarseh <utkarseh@gmail.com>
2026-09-23 23:33:31 +0530

    sys module

--- pythonModules.py/SYSmodule.py
+++ pythonModules.py/SYSmodule.py
@@ -3,5 +3,9 @@ inputStatement=sys.stdin.readline(10)
 print(inputStatement)
 sys.stdout.write("this is a statement")
 print(sys.version)
-#headache
+#headachee
+
 print("headache")
+a=2
+b=2
+print(a+b)
